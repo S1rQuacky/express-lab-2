516deb1fd9b7633397b8e0425cbcdac60519f8e3
Neide Urbina <neide.n.urbina@gmail.com>
2021-09-17 21:44:11 -0700

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -16,6 +16,35 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(tip.toString())
 })
 
+// Magic 8 ball answer array
+const ballResponse = ["It is certain", 
+    "It is decidedly so", 
+    "Without a doubt", 
+    "Yes definitely",
+    "You may rely on it", 
+    "As I see it yes", 
+    "Most likely", 
+    "Outlook good",
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy try again", 
+    "Ask again later",
+    "Better not tell you now", 
+    "Cannot predict now", 
+    "Concentrate and ask again",
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no",
+    "Outlook not so good", 
+    "Very doubtful"];
+
+const resp8 = Math.floor(Math.random() * ballResponse.length)
+
+app.get('/magic/:question', (req, res) => {
+    const htmlH1 = `<h1>${ballResponse[resp8]}</h1>`
+    res.send((req.params.question) + htmlH1)
+})
+
 app.listen(3000, () => {
     console.log("listening")
 });
